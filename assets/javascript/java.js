9e01736d5f98db0acf5be7e1d5fbc0565630df3c
Ryan Hardeman <Ryan Hardeman>
2017-06-03 18:38:19 -0400

    assigned gifs to buttons

--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -2,46 +2,47 @@ var subjects =["deadpool", "picard", "wonder woman gadot"],s;
 var limit = '&limit=10';
 var apiKey = "&api_key=dc6zaTOxFJmzC";
 
-// search query to be called by ajax function
-var queryURL = 
-	"https://api.giphy.com/v1/gifs/search?q=" + "wonder woman gadot" + limit + apiKey;
+var gifDiv =[];
 
-//this function creates the buttons when called
+
 function displayButtons() {
-	//increment s until it is no longer less than the subjects list
-	for(s = 0; s < subjects.length; s++) {
-		//create showButtons and append them to the subject-btn
-		showButtons = $('<div class="col col-xs-2 col-btn"><button class="btn btn-default btn-gif" data-subject="' 
-		+ subjects[s] + '"><span>' + subjects[s] + '</span></button></div>');
-		$(".subject-btn").append(showButtons);
-	};
-};
 
-//api call all day every day
-$.ajax({
-	url: queryURL,
-	method: 'GET'
-}).done(function(response){
+  for (var s = 0; s < subjects.length; s++) {
+      var d = $('<button .btn-info>');
+      d.addClass('subjects');
+      d.attr('data', subjects[s]);
+      d.text(subjects[s]);
+      d.attr('data-state', $(this).attr('data-state', 'animate'));
+      $('#subjectBtn').append(d);
+    }
+
+    $("button").on("click",function(){
+	$(".gifContainer").empty();
+	var search = $(this).attr("data");
+	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + search + limit + apiKey;
+
+	$.ajax({
+		url: queryURL,
+		method: 'GET'
+	}).done(function(response){
 	console.log(response);
 
-	var results = response.data;
+		var results = response.data;
 
-    for (var i = 0; i < results.length; i++){
-    	var gifDiv = $("<div class='gif'>");
+		    for (var i = 0; i < results.length; i++){
+		    	gifDiv = $("<div class='gif'>");
 
-    	var gifImage = $("<img>");
-    	gifImage.attr("src", results[i].images.fixed_height.url);
-    	
-    	gifDiv.prepend(gifImage);
+		    	var gifImage = $("<img>");
+		    	gifImage.attr("src", results[i].images.fixed_height.url);
+		    	
+		    	gifDiv.prepend(gifImage);
 
-    	$(".gifContainer").prepend(gifDiv);
+		  	  	$(".gifContainer").prepend(gifDiv);
 
-    };
-    // var gif = $("<img>").attr("src", gifURL);
-    // $(".gifContainer").html(gif);
-
-});
-
+    		}; // end of for loop
+		}); // end of done function
+	});// end of on click function
+} // end of display function
 
 //on document ready call the displayButtons function to create subject buttons
 $(document).ready(function() {
