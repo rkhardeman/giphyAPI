8bd5ba3b31d01913f4f93a6b7af450ecff95799b
Ryan Hardeman <Ryan Hardeman>
2017-06-04 15:51:52 -0400

    added ability to pause gifs, gifs are still by default

--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -1,5 +1,5 @@
-var subjects =["deadpool", "picard", "wonder woman gadot"],s;
-var limit = '&limit=10';
+var subjects =["Deadpool", "Thor", "Wonder Woman (Gadot)"],s;
+var limit = "&limit=10";
 var apiKey = "&api_key=dc6zaTOxFJmzC";
 
 var gifDiv =[];
@@ -8,12 +8,12 @@ var gifDiv =[];
 function displayButtons() {
 
   for (var s = 0; s < subjects.length; s++) {
-      var d = $('<button .btn-info>');
-      d.addClass('subjects');
-      d.attr('data', subjects[s]);
+      var d = $("<button .btn-info>");
+      d.addClass("subjects");
+      d.attr("data", subjects[s]);
       d.text(subjects[s]);
-      d.attr('data-state', $(this).attr('data-state', 'animate'));
-      $('#subjectBtn').append(d);
+      d.attr("data-state", $(this).attr("data-state", "animate"));
+      $("#subjectBtn").append(d);
     }
 
     $("button").on("click",function(){
@@ -33,16 +33,33 @@ function displayButtons() {
 		    	gifDiv = $("<div class='gif'>");
 
 		    	var gifImage = $("<img>");
-		    	gifImage.attr("src", results[i].images.fixed_height.url);
+		    	gifImage.attr("src", results[i].images.fixed_height_still.url);
+		    	gifImage.attr("data-still", results[i].images.fixed_height_still.url);
+		    	gifImage.attr("data-animate", results[i].images.fixed_height.url);
+		    	gifImage.attr("data-state", "still");
 		    	
 		    	gifDiv.prepend(gifImage);
 
 		  	  	$(".gifContainer").prepend(gifDiv);
 
+		  	  	$("img").on('click', function(){
+				      
+				      var state = $(this).attr("data-state");
+
+				      if (state === 'still') {
+				        $(this).attr('src', $(this).data('animate'));
+				        $(this).attr('data-state', 'animate');
+				      } else {
+				        $(this).attr('src', $(this).data('still'));
+				        $(this).attr('data-state', 'still');
+				      }
+				    });// end of img on click
+
     		}; // end of for loop
-		}); // end of done function
-	});// end of on click function
-} // end of display function
+		}); // end of ajax/done
+	});// end of on click event
+} // end of display buttons
+
 
 //on document ready call the displayButtons function to create subject buttons
 $(document).ready(function() {
