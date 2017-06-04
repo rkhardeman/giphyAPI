f71468c573a280f7caac400d8488c98f8c0384cf
Ryan Hardeman <Ryan Hardeman>
2017-06-04 16:42:09 -0400

    addind margin on gifElement divs

--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -12,7 +12,7 @@ function displayButtons() {
       d.addClass("subjects");
       d.attr("data", subjects[s]);
       d.text(subjects[s]);
-      d.attr("data-state", $(this).attr("data-state", "animate"));
+      d.attr("data-state", $(this).attr("data-state", "still"));
       $("#subjectBtn").append(d);
     }
 
@@ -30,9 +30,9 @@ function displayButtons() {
 		var results = response.data;
 
 		    for (var i = 0; i < results.length; i++){
-		    	gifDiv = $("<div class='gif'>");
+		    	gifDiv = $("<div class='gifElement'>");
 
-		    	var gifImage = $("<img>");
+		    	var gifImage = $("<img class=gifClick>");
 		    	gifImage.attr("src", results[i].images.fixed_height_still.url);
 		    	gifImage.attr("data-still", results[i].images.fixed_height_still.url);
 		    	gifImage.attr("data-animate", results[i].images.fixed_height.url);
@@ -42,16 +42,16 @@ function displayButtons() {
 
 		  	  	$(".gifContainer").prepend(gifDiv);
 
-		  	  	$("img").on('click', function(){
+		  	  	$(".gifClick").on("click", function(){
 				      
 				      var state = $(this).attr("data-state");
 
-				      if (state === 'still') {
-				        $(this).attr('src', $(this).data('animate'));
-				        $(this).attr('data-state', 'animate');
+				      if (state == "still") {
+				        $(this).attr("src", $(this).data("animate"));
+				        $(this).attr("data-state", "animate");
 				      } else {
-				        $(this).attr('src', $(this).data('still'));
-				        $(this).attr('data-state', 'still');
+				        $(this).attr("src", $(this).data("still"));
+				        $(this).attr("data-state", "still");
 				      }
 				    });// end of img on click
 
@@ -60,7 +60,7 @@ function displayButtons() {
 	});// end of on click event
 } // end of display buttons
 
-
+ 
 //on document ready call the displayButtons function to create subject buttons
 $(document).ready(function() {
 	displayButtons();
